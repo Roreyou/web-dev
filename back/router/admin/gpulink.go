6af8d3ac528689ca22ee2c21ebc97e5a491208de
jelly <1602313640@qq.com>
2023-04-11 14:36:15 +0800

    Update gpulink.go

--- back/router/admin/gpulink.go
+++ back/router/admin/gpulink.go
@@ -1,12 +1,18 @@
 package admin
 
 import (
+	AdminControllers "back/controller"
+	"back/middlewares"
+
 	"github.com/gin-gonic/gin"
 )
 
 func gpulinkroute(r *gin.Engine) {
 	linkrouter := r.Group("/linkgpu")
+	gpuC := &AdminControllers.Gpu           //控制器
+	linkrouter.Use(middlewares.AuthToken()) //须认证
 	{
+		linkrouter.GET("/item", gpuC.show) //获取可用机器列表
 
 	}
 
