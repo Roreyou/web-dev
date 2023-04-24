ffea25215cc2c201b123018412449c77b8604685
Caroline113 <1412585094@qq.com>
2023-04-24 15:26:55 +0800

    增加里测试中间件的路由

--- back/router/SetupRoute.go
+++ back/router/SetupRoute.go
@@ -1,16 +1,19 @@
 package router
 
 import (
+	"back/middlewares"
 	"back/router/admin"
 
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
-	r := gin.Default()      // 创建一个gin引擎
-	admin.AdminInfoRoute(r) // 调用我的adminIndoRoute来注册中间件auth，并创建一个路由，访问admin的个人信息
-	admin.GpulinkRoute(r)
+	r := gin.Default() // 创建一个gin引擎
+	r.Use(middlewares.AuthToken())
 	GethtmlRouter(r)
 	CheckUserRouter(r)
+	TestMiddlerRouter(r)
+	admin.AdminInfoRoute(r) // 调用我的adminIndoRoute来注册中间件auth，并创建一个路由，访问admin的个人信息
+	admin.GpulinkRoute(r)
 	return r
 }
