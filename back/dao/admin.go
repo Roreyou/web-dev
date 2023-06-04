94fc1c8434c4ca1ba96be2f0f9595d4590ecf4ef
ou-rongyi <1459472280@qq.com>
2023-06-04 16:37:02 +0800

    增加管理员显示所有用户数据的功能

--- back/dao/admin.go
+++ back/dao/admin.go
@@ -3,35 +3,61 @@ package dao
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
-/*
-	func add_user(db *gorm.DB) {
-		db.AutoMigrate(&User_Info{})
-		//插入:
-		hdmpassword := "hdm"
-		lxhpassword := "lxh1"
+func Add_user(c *gin.Context) bool {
+	db := Openmysql()
+	db.AutoMigrate(&User_Info{})
+	//插入id:
+	userid := c.PostForm("user_id")    //返回的是string类型
+	user_id, _ := strconv.Atoi(userid) //要转化成int类型
 
-		hashedPassword_hdm := sha256.Sum256([]byte(hdmpassword))
-		get_hdmPassword := hex.EncodeToString(hashedPassword_hdm[:])
+	userpassword := "123456"
+	//插入user_name
+	user_name := c.PostForm("user_name") //返回的是string类型
+	//c插入real_name
+	real_name := c.PostForm("real_name")
 
-		hashedPassword_lxh := sha256.Sum256([]byte(lxhpassword))
-		get_lxhPassword := hex.EncodeToString(hashedPassword_lxh[:])
+	hashedPassword_hdm := sha256.Sum256([]byte(userpassword))
+	save_Password := hex.EncodeToString(hashedPassword_hdm[:])
 
-		u1 := User_Info{1, "小明", "黄大明", get_hdmPassword}
-		u2 := User_Info{2, "小红", "刘大红", get_lxhPassword}
-		// 创建记录
+	u1 := User_Info{user_id, user_name, real_name, save_Password}
+	// 创建记录
+	db.Table("user_info").Create(&u1)
+	fmt.Println(u1)
+	fmt.Println("完成！")
 
-		db.Table("user_info").Create(&u1)
-		db.Table("user_info").Create(&u2)
+	return true
+}
 
-		fmt.Println("完成！")
+func Delete_usr(c *gin.Context) bool {
+	db := Openmysql()
+	db.AutoMigrate(&User_Info{})
+	userid := c.PostForm("user_id")    //返回的是string类型
+	user_id, _ := strconv.Atoi(userid) //要转化成int类型
+	fmt.Println(user_id)
+	// 查询要删除的用户是否存在
+	var user User_Info
+	result := db.Table("user_info").Where("user_id = ?", user_id).First(&user)
+	fmt.Println(user)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return false
 	}
-*/
+
+	// 删除用户数据
+	db.Debug().Table("user_info").Where("user_id = ?", user_id).Delete(&user)
+	//db.Table("user_info").Delete(&user)
+	db.Close()
+	return true
+
+}
+
 func Init_password(c *gin.Context) bool {
 	db := Openmysql()
 	userid_string := c.PostForm("user_id")
@@ -51,4 +77,12 @@ func Init_password(c *gin.Context) bool {
 	} else {
 		return true
 	}
+}
+
+func Show_alluser(c *gin.Context) []User_Info {
+	db := Openmysql()
+	var saveInfo []User_Info
+	db.Table("user_info").Find(&saveInfo)
+	fmt.Println(saveInfo)
+	return saveInfo
 }
--- back/router/admin/admin.go
+++ back/router/admin/admin.go
@@ -11,3 +11,21 @@ func InitPasswordRoute(r *gin.Engine) {
 	r.LoadHTMLFiles("./no_right.html")                                     //调用中间件，用于进行权限认证
 	InitPRouter.POST("/init_password", controller.InitPassword_Controller) //开始响应，在我们的控制器文件中调用我的处理函数
 }
+
+func Delete_usrRoute(r *gin.Engine) {
+	InitPRouter := r.Group("/admin")
+	r.LoadHTMLFiles("./no_right.html")                               //调用中间件，用于进行权限认证
+	InitPRouter.POST("/delete_user", controller.Delete_usrContrller) //开始响应，在我们的控制器文件中调用我的处理函数
+}
+
+func Add_usrRoute(r *gin.Engine) {
+	InitPRouter := r.Group("/admin")
+	r.LoadHTMLFiles("./no_right.html")                         //调用中间件，用于进行权限认证
+	InitPRouter.POST("/add_user", controller.Add_usrContrller) //开始响应，在我们的控制器文件中调用我的处理函数
+}
+
+func Show_usrRoute(r *gin.Engine) {
+	InitPRouter := r.Group("/admin")
+	r.LoadHTMLFiles("./no_right.html")                               //调用中间件，用于进行权限认证
+	InitPRouter.POST("/show_user", controller.Show_alluserContrller) //开始响应，在我们的控制器文件中调用我的处理函数
+}
